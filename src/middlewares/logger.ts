1d7b658f8a3dd71d8ceedf94026ad3435fcb7ccc
Pololops <paul.rigaudeau@me.com>
2022-10-31 20:47:27 +0100

    delete console logger middleware

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -4,9 +4,7 @@ import type { RootState } from '../state';
 import type { Actions } from '../actions';
 
 const logger: Middleware<{}, RootState> = (store) => (next: Dispatch<Actions>) => (action: Actions) => {
-    console.log('LOGGER : ', action);
     return next(action);
-
 }
 
 export default logger;
