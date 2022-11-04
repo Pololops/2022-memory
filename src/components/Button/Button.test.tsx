9a57f0345f695686d792d51ae15c2c41c0e1ad8f
Pololops <paul.rigaudeau@me.com>
2022-11-04 12:40:54 +0100

    click test added

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Button from './Button';
 
+const handleClick = jest.fn();
 const setup = () => render(
     <Button 
       text="test" 
-      onClick={(): string => 'clicked'} 
+      onClick={() => handleClick()} 
     />
 );
 
@@ -17,5 +19,12 @@ describe('Button component', () => {
   it('should render the text "test" into the button', () => {
     setup();
     expect(screen.getByText('test')).toBeInTheDocument();
+  });
+
+  it('should call onClick prop when clicked', () => {
+    setup();
+
+    fireEvent.click(screen.getByRole('button'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
